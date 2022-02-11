a46f0af86f5df03a656c3ccea62798836d20dafb
Juri <juri@mr-j.it>
2022-02-11 17:13:11 +0100

    menu

---
+++ resources/views/layouts/menu-left.blade.php
@@ -0,0 +1,15 @@
+<ul class="navbar-nav mr-auto">
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Cassa') }}</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Magazzino') }}</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Anagrafiche') }}</a>
+    </li>
+
+</ul>
