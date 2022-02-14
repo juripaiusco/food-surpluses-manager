b5a9b6d34f9475b1a1dadc928c1394a7bfb2a521
Juri <juri@mr-j.it>
2022-02-14 17:23:59 +0100

    Init products module

--- app/Http/Controllers/Product.php
+++ app/Http/Controllers/Product.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Store extends Controller
+class Product extends Controller
 {
     /**
      * Display a listing of the resource.
