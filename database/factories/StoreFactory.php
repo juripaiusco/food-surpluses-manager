4eab2ba2b6a1764d36b6a3735f720ade69a28444
Juri <juri@mr-j.it>
2022-03-04 15:46:00 +0100

    Insert data store

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -5,7 +5,7 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model\Product::class, function (Faker $faker) {
+$factory->define(Model\Store::class, function (Faker $faker) {
 
     $product = new \App\Http\Controllers\Product();
     $type_array = $product->type_array;
@@ -14,10 +14,8 @@ $factory->define(Model\Product::class, function (Faker $faker) {
 
     return [
         'cod' => \Illuminate\Support\Str::random(5),
-        'name' => $faker->colorName,
-        'type' => $type_array[$random_id_type],
-        'points' => random_int(1, 20),
-        'kg' => $type_array[$random_id_type] == 'fead no' ? NULL : random_int(1, 5),
-        'amount' => 1,
+        'kg' => $type_array[$random_id_type] == 'fead no' ? NULL : random_int(10, 100),
+        'amount' => random_int(1, 20),
+        'date' => $faker->dateTimeBetween('-30 days', 'now')
     ];
 });
--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -11,6 +11,15 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Model\Product::class, 15)->create();
+        factory(\App\Model\Product::class, 15)->create()
+            ->each(function ($product) {
+
+                factory(\App\Model\Store::class, random_int(1, 3))->create([
+                    'product_id' => $product->id,
+                    'cod' => $product->cod,
+                    'kg' => $product->type == 'fead no' ? NULL : random_int(10, 100)
+                ]);
+
+            });
     }
 }
