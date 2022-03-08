0bcdd82bf57e70a736ffee43b23c2a3f742a42cf
Juri <juri@mr-j.it>
2022-03-08 18:23:20 +0100

    bootstrap custom-switch

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -6,6 +6,8 @@
 
         @csrf
 
+        <h2>Dati utente</h2>
+
         <div class="row">
             <div class="col">
 
@@ -52,6 +54,15 @@
                     @endif>
             </div>
         @endif
+
+        <br>
+
+        <h2>Moduli attivi</h2>
+
+        <div class="custom-control custom-switch custom-switch-md form-control-lg">
+            <input type="checkbox" class="custom-control-input" id="shop">
+            <label class="custom-control-label" for="shop">Cassa</label>
+        </div>
 
         <div class="text-right">
 
