90fa04d6c23dedccf7e48a62a85a57c6803bffaf
Juri <juri@mr-j.it>
2023-03-24 21:43:54 +0100

    Dashboard layout

---
+++ resources/js/Components/Dashboard/ItemData.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+const props = defineProps({
+    data: Object
+});
+
+</script>
+
+<template>
+
+    <div v-if="data" class="border-2 rounded-md p-6">
+
+        <div class="inline-flex items-center w-full">
+
+            <div class="text-center align-middle w-1/2">
+
+                <span class="text-3xl">
+                    {{ data.count }}
+                </span>
+                <br>
+                {{ data.label }}
+
+            </div>
+            <div class="text-center align-middle w-1/2">
+
+                <div class="w-[60px] h-[60px] m-auto">
+                    <slot />
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -9,7 +9,7 @@ import { Link } from '@inertiajs/vue3';
             <Link href="/">
                 <ApplicationLogo class="w-20 h-20 fill-current text-gray-500 m-auto" />
                 <br>
-                <h1 class="text-3xl">Emporio<span class="font-bold">App</span></h1>
+                <h1 class="text-3xl dark:text-white">Emporio<span class="font-bold">App</span></h1>
             </Link>
         </div>
 
