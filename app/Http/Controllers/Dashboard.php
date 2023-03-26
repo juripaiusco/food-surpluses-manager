9b44d637d5f1d80170e91e94c42c2293fafe4802
Juri <juri@mr-j.it>
2023-03-26 10:19:54 +0200

    Dashboard new data

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use App\Models\Order;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
@@ -18,15 +19,38 @@ class Dashboard extends Controller
 
     public function index()
     {
-        $products = Product::count();
+        // Ultimo ordine
+        $order_latest = Order::all()->last();
 
-        $customers = Customer::count();
+        // Formatto la data dell'ultimo ordine
+        $order_latest_day = Carbon::parse($order_latest->date)->format('Y-m-d');
+        $order_latest_day_string = Carbon::parse($order_latest->date)->format('d/m/Y');
 
-        $orders = Order::count();
+        // Prendo i dati degli ordini dell'ultimo giorno di vendita
+        $order_last_day = Order::with('customer')
+            ->where('date', 'LIKE', $order_latest_day . '%');
+        $order_last_day_data = $order_last_day->get();
 
-        $points = Order::select([
-            DB::raw('SUM(points) AS points_total'),
-        ])->first();
+        // Conto quanti ordini fatti l'ultimo giorno di vendita
+        $orders_count = $order_last_day->count();
+
+        // Conto i prodotti venduti l'ultimo giorno di vendita
+        $products_count = 0;
+        foreach ($order_last_day_data as $order) {
+            $products_count += count(json_decode($order->json_products));
+        }
+
+        // Conto quanti punti usati l'ultimo giorno di vendita
+        $points_count = $order_last_day
+            ->select([
+                DB::raw('SUM(points) AS points_total'),
+            ])->first();
+
+        // Conto i clienti dell'ultimo giorno di vendita
+        $people_count = 0;
+        foreach ($order_last_day_data as $order) {
+            $people_count += $order->customer->family_number;
+        }
 
         // - - - - - -
 
@@ -42,9 +66,9 @@ class Dashboard extends Controller
             ->get();*/
 
         $orders_today = Order::query();
+        $orders_today->where('date', 'LIKE', $order_latest_day . '%');
 
         if (request('s')) {
-            $orders_today->where('date', 'like', '%' . request('s') . '%');
             $orders_today->orWhere('reference', request('s'));
             $orders_today->orWhere('json_customer', 'like', '%' . request('s') . '%');
             $orders_today->orWhere('points', 'like', '%' . request('s') . '%');
@@ -56,12 +80,15 @@ class Dashboard extends Controller
 
         $orders_today = $orders_today->paginate(5)->withQueryString();
 
+        // - - - - - -
+
         return Inertia::render('Dashboard', [
-            'products_count' => $products,
-            'customers_count' => $customers,
-            'orders_count' => $orders,
-            'points_count' => $points->points_total,
+            'products_count' => $products_count,
+            'people_count' => $people_count,
+            'orders_count' => $orders_count,
+            'points_count' => $points_count->points_total,
             'orders_today' => $orders_today,
+            'order_latest_day_string' => $order_latest_day_string,
             'filters' => request()->all(['s', 'orderby', 'ordertype'])
         ]);
     }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,11 +5,12 @@ import ItemData from "@/PagesComponents/Dashboard/ItemData.vue";
 import Table from "@/Components/Table/Table.vue";
 
 defineProps({
-    products_count: Number,
-    customers_count: Number,
     orders_count: Number,
+    products_count: Number,
     points_count: Number,
+    people_count: Number,
     orders_today: Object,
+    order_latest_day_string: String,
     filters: Object,
 });
 
@@ -20,7 +21,16 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
+            <div class="inline-flex w-full">
+                <div class="w-1/2">
+                    <h2 class="text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                        <span class="font-semibold">Dashboard</span>
+                    </h2>
+                </div>
+                <div class="text-right w-1/2">
+                    <span class="text-sm">Ultima spesa {{ order_latest_day_string }}</span>
+                </div>
+            </div>
         </template>
 
         <div class="container mt-8 p-3 bg-white dark:bg-gray-800 dark:text-white shadow-sm rounded-lg">
@@ -28,34 +38,6 @@ defineProps({
             <div class="p-6">
 
                 <div class="row">
-                    <div class="col">
-
-                        <ItemData class="border-yellow-400" :data="{
-                                    label: 'Prodotti',
-                                    count: products_count
-                                }">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                            </svg>
-
-                        </ItemData>
-
-                    </div>
-                    <div class="col">
-
-                        <ItemData class="border-blue-400" :data="{
-                                    label: 'Tessere',
-                                    count: customers_count
-                                }">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
-                            </svg>
-
-                        </ItemData>
-
-                    </div>
                     <div class="col">
 
                         <ItemData class="border-gray-400" :data="{
@@ -72,13 +54,41 @@ defineProps({
                     </div>
                     <div class="col">
 
+                        <ItemData class="border-yellow-400" :data="{
+                                    label: 'Prodotti',
+                                    count: products_count
+                                }">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+
+                        </ItemData>
+
+                    </div>
+                    <div class="col">
+
                         <ItemData class="border-green-400" :data="{
-                                    label: 'Punti',
+                                    label: 'Punti usati',
                                     count: points_count
                                 }">
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+
+                        </ItemData>
+
+                    </div>
+                    <div class="col">
+
+                        <ItemData class="border-blue-400" :data="{
+                                    label: 'Persone aiutate',
+                                    count: people_count
+                                }">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                             </svg>
 
                         </ItemData>
