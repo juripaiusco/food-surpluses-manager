c1ec1f7ee632fef83028e4af3bb1401386279754
Juri <juri@mr-j.it>
2023-03-28 17:27:59 +0200

    Fix btn delete

--- resources/js/Components/Table/Table.vue
+++ resources/js/Components/Table/Table.vue
@@ -35,20 +35,11 @@ defineProps({
 
                     <template v-if="struct.btnEdit === true || struct.btnDel === true">
 
-<<<<<<< HEAD
-                            <Link class="btn btn-sm"
-                                  :href="struct.btnDel === true ? '#' : route(struct.route, d.id)"
-                                  :data-bs-toggle="struct.btnDel === true ? 'modal' : ''"
-                                  :data-bs-target="struct.btnDel === true ? '#deleteModal' : ''"
-                                  @click="struct.btnDel === true ? passDataToModal(struct, d) : ''"
-                                  :class="{
-=======
                         <Link class="btn btn-sm"
                               :href="struct.btnDel === true ? '#' : route(struct.route, d.id)"
                               :class="{
->>>>>>> parent of 7e911bd (Delete modal request)
-                                'btn-warning': struct.btnEdit === true,
-                                'btn-danger': struct.btnDel === true
+                                  'btn-warning': struct.btnEdit === true,
+                                  'btn-danger': struct.btnDel === true
                               }">
 
                             <svg v-if="struct.btnEdit === true"
@@ -56,43 +47,6 @@ defineProps({
                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                             </svg>
-<<<<<<< HEAD
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Vuoi eliminare?
-                    </div>
-                    <div class="modal-footer">
-                        <div class="inline-flex w-full">
-                            <div class="w-1/2 mr-1">
-
-                                <button type="button"
-                                        class="btn btn-secondary w-full"
-                                        data-bs-dismiss="modal">
-                                    No
-                                </button>
-
-                            </div>
-                            <div class="w-1/2 ml-1">
-
-<!--                                <Link class="btn btn-danger w-full"
-                                      :href="structModal.route ? route(structModal.route, dataModal.id) : '#'">
-                                    Sì, elimina
-                                </Link>-->
-
-                                <button class="btn btn-danger w-full"
-                                        @click="">
-                                    Sì, elimina
-                                </button>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-=======
->>>>>>> parent of 7e911bd (Delete modal request)
 
                             <svg v-if="struct.btnDel === true"
                                  class="w-4 h-4"
@@ -112,23 +66,3 @@ defineProps({
     </table>
 
 </template>
-
-<script>
-export default {
-    data () {
-        return {
-            structModal: Object,
-            dataModal: Object,
-        }
-    },
-    methods: {
-        passDataToModal(struct, d) {
-            this.dataModal = d;
-            this.structModal = struct;
-        },
-        modalHide() {
-            let deleteModal = document.getElementById('deleteModal');
-        }
-    }
-}
-</script>
