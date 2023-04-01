97d1d7ed482266e9161b49f3fb8eded142ce8667
Juri <juri@mr-j.it>
2023-04-01 19:43:24 +0200

    Update TableTh.vue

--- resources/js/Components/Table/TableTh.vue
+++ resources/js/Components/Table/TableTh.vue
@@ -21,12 +21,12 @@ defineProps({
                 </div>
 
                 <div class="w-[12px] h-[12px]"
-                     :class="{ invisible: params.orderby != struct.field }">
-                    <svg :class="{ 'text-gray-300': params.ordertype === 'desc' }"
+                     :class="{ 'invisible': params.orderby !== struct.field }">
+                    <svg :class="{ 'text-gray-300': params.ordertype === 'desc' && params.orderby === struct.field }"
                          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
                     </svg>
-                    <svg :class="{ 'text-gray-300': params.ordertype === 'asc' }"
+                    <svg :class="{ 'text-gray-300': params.ordertype === 'asc' && params.orderby === struct.field }"
                          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                     </svg>
