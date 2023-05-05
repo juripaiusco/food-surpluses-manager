78274e7006adf1a6b33f4af275a1a4ea4fca73a2
Juri <juri@mr-j.it>
2023-05-05 18:33:48 +0200

    Change icon

--- resources/js/Pages/Orders/List.vue
+++ resources/js/Pages/Orders/List.vue
@@ -91,7 +91,7 @@ defineProps({
 
                                 if (parseInt(timeStampDate(d.date)) === parseInt(today)) {
                                     html += '<div class=\'btn btn-warning btn-sm\'>';
-                                    html += '<svg class=\'w-4 h-4\' xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10\' /></svg>';
+                                    html += '<svg class=\'w-4 h-4\' xmlns=\'http://www.w3.org/2000/svg\' fill=\'none\' viewBox=\'0 0 24 24\' stroke-width=\'1.5\' stroke=\'currentColor\'><path stroke-linecap=\'round\' stroke-linejoin=\'round\' d=\'M13.5 10.5V6.75a4.5 4.5 0 119 0v3.75M3.75 21.75h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H3.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z\' /></svg>';
                                     html += '</div>';
                                 }
 
