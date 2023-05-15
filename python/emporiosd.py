836725218315b646b1403c74acc75666d0ded605
Juri <juri@mr-j.it>
2023-05-15 12:46:36 +0200

    Python sendmail

--- python/emporiosd.py
+++ python/emporiosd.py
@@ -2,48 +2,55 @@ import os
 import sys
 import mysql.connector as mariadb
 import pandas as pd
+from dotenv import load_dotenv
+from pathlib import Path
+import smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from datetime import datetime
 
-""" Try connection """
+""" VAR - SET VARIABLES """
+pathExcel = './excel'
+load_dotenv(Path('../.env'))
+
+""" FNC - GET DATA FROM DB """
+def getData(query, conn):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    dataTables = cursor.fetchall()
+
+    return dataTables
+
+""" ------------------------------------------------------------ """
+
+""" TRY CONNECTION """
 try:
     conn = mariadb.connect(
-        user='root',
-        password="secret",
-        database='emporiosd_app',
-        host='db',
-        port='3306'
+        user=os.getenv('DB_USERNAME'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
     )
 except mariadb.errors as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
 
-pathToSave = './excel'
-
 """ Select TABLES list """
-query = 'SHOW TABLES'
-
-cursor = conn.cursor()
-cursor.execute(query)
-dataTables = cursor.fetchall()
+dataTables = getData('SHOW TABLES', conn)
 
 for dataTable in dataTables:
 
     tbl = dataTable[0]
 
-    """ Select Table Columns """
-    query = f'SHOW COLUMNS FROM {tbl}'
+    """ Select Table Columns - Data """
+    dataTableColumns = getData('SHOW COLUMNS FROM ' + tbl, conn)
 
-    cursor = conn.cursor()
-    cursor.execute(query)
-    dataTableColumns = cursor.fetchall()
-    df_dataTableColumns = pd.DataFrame(dataTableColumns)
-
-    """ Select Table Data """
-    query = f'SELECT * FROM {tbl} LIMIT 0, 2'
-
-    cursor = conn.cursor()
-    cursor.execute(query)
-    dataTable = cursor.fetchall()
-    df_dataTable = pd.DataFrame(dataTable)
+    """ Select Table Data - DataFrame """
+    df_dataTable = pd.DataFrame(getData('SELECT * FROM ' + tbl, conn))
 
     """ Eseguo l'esportazione in Excel """
     row = [[]]
@@ -54,11 +61,68 @@ for dataTable in dataTables:
         c += 1
 
     df_tableHeader = pd.DataFrame(row)
-
     df_dataWithHeader = pd.concat([df_tableHeader, df_dataTable])
-    df_dataWithHeader.to_excel(f'{pathToSave}/{tbl}.xlsx')
+    df_dataWithHeader.to_excel(f'{pathExcel}/{tbl}.xlsx')
+
+""" ------------------------------------------------------------ """
+
+""" EXCEL ZIP - COMPRIMO I FILE EXCEL """
+os.system(f'zip -r {pathExcel}.zip {pathExcel}')
+
+""" EXCEL FILE - RIMUOVO I FILE EXCEL """
+for file in os.listdir(pathExcel):
+    os.remove(pathExcel + '/' + file)
+
+""" ------------------------------------------------------------ """
+
+""" EMAIL - INVIO MAIL CON ALLEGATO """
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(os.getenv('MAIL_HOST'), os.getenv('MAIL_PORT'))
+    server.ehlo() # Can be omitted
+    server.starttls(context=ssl.create_default_context()) # Secure the connection
+    server.ehlo() # Can be omitted
+    server.login(os.getenv('MAIL_USERNAME'), os.getenv('MAIL_PASSWORD'))
+    # TODO: Send email here
+
+    """ ------------------------- """
+    email = getData('SELECT value FROM settings WHERE name="report_email"', conn)[0][0]
+    subject = '[EmporioApp ' + os.getenv('APP_DOMAIN') + '] - '
+    subject += 'DB2Excel del ' + datetime.today().strftime('%d/%m/%Y - %H:%I:%S')
+
+    message = MIMEMultipart()
+    message['Subject'] = subject
+    message['From'] = 'emporioapp@emporiosd.it'
+    message['To'] = email
+
+    text = """\
+    Ciao,
+
+    trovi in allegato il database in formato Excel
+
+    Buon lavoro,
+
+    Bot EmporioApp
+    """
+
+    message.attach(MIMEText(text, 'plain'))
+
+    filename = pathExcel + '.zip'
+    with open(filename, 'rb') as file:
+        message.attach(MIMEApplication(file.read(), Name='Excel.zip'))
+
+    email_message = message.as_string()
+    """ ------------------------- """
+
+    server.sendmail('emporioapp@emporiosd.it', email, email_message)
+
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
+
+os.remove(pathExcel + '.zip')
+""" ------------------------------------------------------------ """
 
 conn.close()
-
-""" Comprimo i file Excel """
-os.system(f'zip -r {pathToSave}.zip {pathToSave}')
