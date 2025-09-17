e7badb4b1c95816c57f025e0b6e9af75ce69dafe
Juri <juri@mr-j.it>
2025-09-17 11:53:08 +0200

    Nav tab

--- resources/js/Pages/Settings/Form.vue
+++ resources/js/Pages/Settings/Form.vue
@@ -42,48 +42,98 @@ const form = useForm(dataForm);
                 {{ msg }}
             </div>
 
-            <h2 class="text-3xl mb-2">Cassa</h2>
-
             <form @submit.prevent="form.post(route('settings.update'))">
 
-                <label for="shop_btn" class="form-label">
-                    Inserisci il codice prodotto, separato da virgola "," (senza doppi apici).
-                </label>
-                <textarea id="shop_btn"
-                          name="shop_btn"
-                          class="form-control mb-4 h-[178px]"
-                          v-model="form.shop_btn"></textarea>
+                <nav>
+                    <div class="nav nav-underline nav-fill" id="nav-tab" role="tablist">
 
-                <div class="form-check">
-                    <input class="form-check-input"
-                           type="checkbox"
-                           name="shop_ctrl_points"
-                           id="shop_ctrl_points"
-                           true-value="1"
-                           false-value="0"
-                           v-model="form.shop_ctrl_points">
-                    <label class="form-check-label" for="shop_ctrl_points">Controllo punti spesa</label>
+                        <button class="nav-link active"
+                                id="nav-general-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-general"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-general"
+                                aria-selected="true">
+                            Generale
+                        </button>
+
+                        <button class="nav-link"
+                                id="nav-customers_mod_jobs-tab"
+                                data-bs-toggle="tab"
+                                data-bs-target="#nav-customers_mod_jobs"
+                                type="button"
+                                role="tab"
+                                aria-controls="nav-customers_mod_jobs"
+                                aria-selected="true">
+                            Modulo Lavoro
+                        </button>
+
+                    </div>
+                </nav>
+
+                <div class="tab-content" id="nav-tabContent">
+
+                    <div class="tab-pane fade show active pt-4"
+                         id="nav-general"
+                         role="tabpanel"
+                         aria-labelledby="nav-general-tab"
+                         tabindex="0">
+
+                        <h2 class="text-3xl mb-2">Cassa</h2>
+
+                        <label for="shop_btn" class="form-label">
+                            Inserisci il codice prodotto, separato da virgola "," (senza doppi apici).
+                        </label>
+                        <textarea id="shop_btn"
+                                  name="shop_btn"
+                                  class="form-control mb-4 h-[178px]"
+                                  v-model="form.shop_btn"></textarea>
+
+                        <div class="form-check">
+                            <input class="form-check-input"
+                                   type="checkbox"
+                                   name="shop_ctrl_points"
+                                   id="shop_ctrl_points"
+                                   true-value="1"
+                                   false-value="0"
+                                   v-model="form.shop_ctrl_points">
+                            <label class="form-check-label" for="shop_ctrl_points">Controllo punti spesa</label>
+                        </div>
+
+                        <h2 class="text-3xl mt-8 mb-2">Report</h2>
+
+                        <label for="report_email" class="form-label">
+                            Email invio Report, inserisci le email separato da virgola "," (senza doppi apici).
+                        </label>
+                        <input id="report_email"
+                               name="report_email"
+                               type="text"
+                               class="form-control"
+                               v-model="form.report_email" />
+                        <div class="text-red-500"
+                             v-if="form.errors.report_email">{{ __(form.errors.report_email) }}</div>
+
+                        <h2 class="text-3xl mt-8 mb-2">Export DB2Excel</h2>
+
+                        <Link class="btn btn-info btn-lg"
+                              :href="route('settings.db2excel')">
+                            Export DB
+                        </Link>
+
+                    </div>
+
+                    <div class="tab-pane fade show pt-4"
+                         id="nav-customers_mod_jobs"
+                         role="tabpanel"
+                         aria-labelledby="nav-customers_mod_jobs-tab"
+                         tabindex="0">
+
+                        <h2 class="text-3xl mb-2">Modulo Lavoro - Impostazione campi</h2>
+
+                    </div>
+
                 </div>
-
-                <h2 class="text-3xl mt-8 mb-2">Report</h2>
-
-                <label for="report_email" class="form-label">
-                    Email invio Report, inserisci le email separato da virgola "," (senza doppi apici).
-                </label>
-                <input id="report_email"
-                       name="report_email"
-                       type="text"
-                       class="form-control"
-                       v-model="form.report_email" />
-                <div class="text-red-500"
-                     v-if="form.errors.report_email">{{ __(form.errors.report_email) }}</div>
-
-                <h2 class="text-3xl mt-8 mb-2">Export DB2Excel</h2>
-
-                <Link class="btn btn-info btn-lg"
-                      :href="route('settings.db2excel')">
-                    Export DB
-                </Link>
 
                 <div class="text-right mt-8">
 
