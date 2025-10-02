b05900d5a5348ba731f61016e0bba746cf8ab377
Juri <juri@mr-j.it>
2025-10-02 17:26:06 +0200

    Fix no data dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -41,85 +41,96 @@ class Dashboard extends Controller
             ->orderBy('date', 'DESC')
             ->first();
 
-        // Formatto la data dell'ultimo ordine
-        $orders_latest_date = Carbon::parse($order_latest->date)->format('Y-m-d');
-        $orders_latest_date_string = Carbon::parse($order_latest->date)->format('d/m/Y');
+        if ($order_latest) {
+            // Formatto la data dell'ultimo ordine
+            $orders_latest_date = Carbon::parse($order_latest->date)->format('Y-m-d');
+            $orders_latest_date_string = Carbon::parse($order_latest->date)->format('d/m/Y');
 
-        // ===============================================================
-        request()->validate([
-            'orderby' => ['in:date,reference,customer_name,points'],
-            'ordertype' => ['in:asc,desc']
-        ]);
+            // ===============================================================
+            request()->validate([
+                'orderby' => ['in:date,reference,customer_name,points'],
+                'ordertype' => ['in:asc,desc']
+            ]);
 
-        // Prendo i dati degli ordini dell'ultimo giorno di vendita
-        $order_last_day = Order::with('customer')
-            ->where('date', 'LIKE', $orders_latest_date . '%')
-            ->select()
-            ->addSelect('json_customer->name AS customer_firstname')
-            ->addSelect('json_customer->surname AS customer_lastname')
-            ->addSelect(DB::raw(
-                'CONCAT(
-                    JSON_VALUE(json_customer, \'$.surname\'), \' \', JSON_VALUE(json_customer, \'$.name\')
-                ) AS customer_name'
-            ));
+            // Prendo i dati degli ordini dell'ultimo giorno di vendita
+            $order_last_day = Order::with('customer')
+                ->where('date', 'LIKE', $orders_latest_date . '%')
+                ->select()
+                ->addSelect('json_customer->name AS customer_firstname')
+                ->addSelect('json_customer->surname AS customer_lastname')
+                ->addSelect(DB::raw(
+                    'CONCAT(
+                        JSON_VALUE(json_customer, \'$.surname\'), \' \', JSON_VALUE(json_customer, \'$.name\')
+                    ) AS customer_name'
+                ));
 
-        // Filtro i dati degli ordini dell'ultimo giorno
-        if (request('s')) {
-            $order_last_day->where(function ($q) {
+            // Filtro i dati degli ordini dell'ultimo giorno
+            if (request('s')) {
+                $order_last_day->where(function ($q) {
 
-                $q->orWhere('reference', 'like', '%' . request('s') . '%');
-                $q->orWhere('json_customer', 'like', '%' . request('s') . '%');
-                $q->orWhere('points', 'like', '%' . request('s') . '%');
+                    $q->orWhere('reference', 'like', '%' . request('s') . '%');
+                    $q->orWhere('json_customer', 'like', '%' . request('s') . '%');
+                    $q->orWhere('points', 'like', '%' . request('s') . '%');
 
-            });
-        }
-
-        // Ordino i dati degli ordini dell'ultimo giorno
-        if (request('orderby') && request('ordertype')) {
-            $order_last_day->orderby(request('orderby'), strtoupper(request('ordertype')));
-        }
-
-        // Prendo i dati degli ordini dell'ultimo giorno
-        $order_last_day_data = $order_last_day->get();
-        // ===============================================================
-
-        // Conto quanti ORDINI fatti l'ultimo giorno di vendita
-        $orders_count = $order_last_day_data->count();
-
-        // Conto i PRODOTTI venduti l'ultimo giorno di vendita
-        $products_count = 0;
-        foreach ($order_last_day_data as $order) {
-            $products_count += count(json_decode($order->json_products));
-        }
-
-        // Conto quanti PUNTI usati l'ultimo giorno di vendita
-        $points_count = 0;
-        foreach ($order_last_day_data as $order) {
-            $points_count += $order->points;
-        }
-
-        // Conto i CLIENTI dell'ultimo giorno di vendita
-        $people_count = 0;
-        $people_count_array = array();
-        foreach ($order_last_day_data as $order) {
-            if (!isset($people_count_array[$order->customer->id])) {
-                $people_count_array[$order->customer->id] = $order->customer->family_number;
-                $people_count += $people_count_array[$order->customer->id];
+                });
             }
-        }
 
-        // Prendo i dati impaginati degli ordini dell'ultimo giorno
-        $order_last_day = $order_last_day->select([
-            'date',
-            'reference',
-            'points',
-        ]);
-        $order_last_day = $order_last_day->addSelect(DB::raw(
-            'CONCAT(
-                    JSON_VALUE(json_customer, \'$.surname\'), \' \', JSON_VALUE(json_customer, \'$.name\')
-                ) AS customer_name'
-        ));
-        $order_last_day_data_paginate = $order_last_day->paginate(env('VIEWS_PAGINATE'))->withQueryString();
+            // Ordino i dati degli ordini dell'ultimo giorno
+            if (request('orderby') && request('ordertype')) {
+                $order_last_day->orderby(request('orderby'), strtoupper(request('ordertype')));
+            }
+
+            // Prendo i dati degli ordini dell'ultimo giorno
+            $order_last_day_data = $order_last_day->get();
+            // ===============================================================
+
+            // Conto quanti ORDINI fatti l'ultimo giorno di vendita
+            $orders_count = $order_last_day_data->count();
+
+            // Conto i PRODOTTI venduti l'ultimo giorno di vendita
+            $products_count = 0;
+            foreach ($order_last_day_data as $order) {
+                $products_count += count(json_decode($order->json_products));
+            }
+
+            // Conto quanti PUNTI usati l'ultimo giorno di vendita
+            $points_count = 0;
+            foreach ($order_last_day_data as $order) {
+                $points_count += $order->points;
+            }
+
+            // Conto i CLIENTI dell'ultimo giorno di vendita
+            $people_count = 0;
+            $people_count_array = array();
+            foreach ($order_last_day_data as $order) {
+                if (!isset($people_count_array[$order->customer->id])) {
+                    $people_count_array[$order->customer->id] = $order->customer->family_number;
+                    $people_count += $people_count_array[$order->customer->id];
+                }
+            }
+
+            // Prendo i dati impaginati degli ordini dell'ultimo giorno
+            $order_last_day = $order_last_day->select([
+                'date',
+                'reference',
+                'points',
+            ]);
+            $order_last_day = $order_last_day->addSelect(DB::raw(
+                'CONCAT(
+                        JSON_VALUE(json_customer, \'$.surname\'), \' \', JSON_VALUE(json_customer, \'$.name\')
+                    ) AS customer_name'
+            ));
+            $order_last_day_data_paginate = $order_last_day->paginate(env('VIEWS_PAGINATE'))->withQueryString();
+
+        } else {
+
+            $products_count = 0;
+            $people_count = 0;
+            $orders_count = 0;
+            $points_count = 0;
+            $order_last_day_data_paginate = [];
+            $orders_latest_date_string = '';
+        }
 
         return Inertia::render('Dashboard', [
             'products_count' => $products_count,
