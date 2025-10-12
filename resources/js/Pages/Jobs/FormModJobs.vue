20384bc805aaab00fdc1ea703defd07e55b3cce1
Juri <juri@mr-j.it>
2025-10-12 16:15:05 +0200

    Change add and remove btn

--- resources/js/Pages/Jobs/FormModJobs.vue
+++ resources/js/Pages/Jobs/FormModJobs.vue
@@ -280,10 +280,19 @@ onBeforeUnmount(() => {
 
                                             <button type="button"
                                                     class="btn btn-danger btn-sm"
-                                                    v-if="sIndex > 0"
+                                                    v-if="sIndex < (dynamicSchemas[index].length - 1)"
                                                     @click="removeSchema(index, schema._id)">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                            </button>
+
+                                            <button type="button"
+                                                    class="btn btn-primary btn-sm"
+                                                    v-if="sIndex === (dynamicSchemas[index].length - 1)"
+                                                    @click="addSchema(index, data.schema)">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                                 </svg>
                                             </button>
 
@@ -294,7 +303,7 @@ onBeforeUnmount(() => {
 
                             </FormKit>
 
-                            <div v-if="data.dynamic === '1'"
+                            <!-- <div v-if="data.dynamic === '1'"
                                  class="text-right">
 
                                 <button type="button"
@@ -306,7 +315,7 @@ onBeforeUnmount(() => {
                                         </span>
                                     </div>
                                 </button>
-                            </div>
+                            </div> -->
 
                         </div>
                     </div>
